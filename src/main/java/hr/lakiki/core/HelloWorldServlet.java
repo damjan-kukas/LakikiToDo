6abc9971fe35e723297eb77b0e81bbfb1337b491
Damjan <damjan.kukas@kapsch.net>
2015-01-18 15:36:29 +0100

    Switched to gradle 2.2.1, added logback dependency, moved HelloWorldServlet.java to another package, logback support.

--- src/main/java/hr/lakiki/core/HelloWorldServlet.java
+++ src/main/java/hr/lakiki/core/HelloWorldServlet.java
@@ -1,19 +1,25 @@
-package core;
+package hr.lakiki.core;
 
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HelloWorldServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldServlet.class);
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
             throws ServletException, IOException {
+
+        logger.debug("Just saying hello through logger!");
         resp.getOutputStream().write("Hello Lucijan.".getBytes());
     }
 }
